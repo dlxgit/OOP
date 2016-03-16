0a6155971ab6482b25dc387f88c01f0c928476c6
Andrey <dayl1ex@gmail.com>
2016-03-16 05:04:04 +0300

    fixed invert

--- invert/main.cpp
+++ invert/main.cpp
@@ -4,14 +4,13 @@
 #include <math.h>
 #include <sstream>
 #include <iomanip>
+#include <array>
 
 
-struct Cofactor
-{
-	double matrix[2][2];
-};
+typedef std::array<std::array<double, 2>, 2> Matrix2x2; //for cofactor
+typedef std::array<std::array<double, 3>, 3> Matrix3x3; //for rest matrixes
 
-void PrintMatrix(const double(&matrix)[3][3])
+void PrintMatrix(const Matrix3x3 & matrix)
 {
 	std::cout << "Inverted Matrix: " << std::endl;
 	
@@ -25,7 +24,7 @@ void PrintMatrix(const double(&matrix)[3][3])
 	}
 }
 
-double ComputeDeterminant(double matrix[][3])
+double ComputeDeterminant(const Matrix3x3 & matrix)
 {
 	return (
 		matrix[0][0] * matrix[1][1] * matrix[2][2] +
@@ -37,9 +36,9 @@ double ComputeDeterminant(double matrix[][3])
 		);
 }
 
-Cofactor ComputeCofactor(const double(&matrix)[3][3], const size_t & i, const size_t & j)
+Matrix2x2 ComputeCofactor(const Matrix3x3 & matrix	, const size_t & i, const size_t & j)
 {
-	Cofactor cofactor;
+	Matrix2x2 cofactor;
 	size_t xPos = 0; //x and y - indexes of Cofactor (for filling)
 	size_t yPos = 0;
 	for (size_t p = 0; p < 3; p++)
@@ -49,7 +48,7 @@ Cofactor ComputeCofactor(const double(&matrix)[3][3], const size_t & i, const si
 		{
 			if (p != i && q != j)
 			{
-				cofactor.matrix[yPos][xPos] = matrix[p][q];
+				cofactor[yPos][xPos] = matrix[p][q];
 				if (xPos == 1)
 				{
 					yPos = 1;
@@ -61,23 +60,25 @@ Cofactor ComputeCofactor(const double(&matrix)[3][3], const size_t & i, const si
 	return cofactor;
 }
 
-double ComputeCellOfInvertedMatrix(const double(&matrix)[3][3], const double & det, const size_t & i, const size_t & j)  //value of [i][j] element of inverted matrix
+double ComputeCellOfInvertedMatrix(const Matrix3x3 & matrix, const double & det, const size_t & i, const size_t & j)  //value of [i][j] element of inverted matrix
 {
-	Cofactor cofactor = ComputeCofactor(matrix, i, j);
-	double cofValue = (cofactor.matrix[0][0] * cofactor.matrix[1][1] - cofactor.matrix[0][1] * cofactor.matrix[1][0]);
+	Matrix2x2 cofactor = ComputeCofactor(matrix, i, j);
+	double cofValue = (cofactor[0][0] * cofactor[1][1] - cofactor[0][1] * cofactor[1][0]);
 	return pow(-1, i + j) * cofValue / det;
 }
 
-bool ComputeInvertedMatrix(const std::string & fileName)
+Matrix3x3 ReadMatrixFromFile(const std::string & fileName, bool & isError)
 {
+	Matrix3x3 matrix = Matrix3x3();
+
+	//reading matrix from inputFile and checking for incorrect dimension
 	std::ifstream inputFile(fileName);
 	if (!inputFile.is_open())
 	{
-		return false;
+		isError = true;
+		return matrix;
 	}
-	
-	//reading matrix from inputFile and checking for incorrect dimension
-	double matrix[3][3];
+
 	std::string line;
 	size_t lineCount = 0;
 	while (std::getline(inputFile, line))
@@ -85,27 +86,39 @@ bool ComputeInvertedMatrix(const std::string & fileName)
 		size_t numCount = 0;
 		double number;
 		std::istringstream iss(line);
-		while (iss >> number && lineCount < 3)
+		while (lineCount < 3 && numCount < 3 && iss >> number)
 		{
 			matrix[lineCount][numCount] = number;
-			if (++numCount > 3)
-			{
-				std::cout << "Error: incorrect number of elements in matrix" << std::endl;
-				return false;
-			}
+			numCount++;
 		}
-		if (numCount < 3)
+		if (numCount != 3)
 		{
 			std::cout << "Error: incorrect number of elements in matrix" << std::endl;
-			return false;
+			isError = true;
+			return matrix;
 		}
 		lineCount++;
 	}
 
-	//in case of empty file
-	if (lineCount == 0)
+	if (lineCount != 3)
 	{
-		return false;
+		isError = true;
+		return matrix;
+	}
+
+	return matrix;
+}
+
+Matrix3x3 ComputeInvertedMatrix(const std::string & fileName, bool & wasError)
+{
+	bool isError = false;
+
+	//ReadMatrix
+	Matrix3x3 matrix = ReadMatrixFromFile(fileName, isError);
+	if (isError)
+	{
+		wasError = true;
+		return matrix;
 	}
 
 	//computing Det
@@ -113,11 +126,12 @@ bool ComputeInvertedMatrix(const std::string & fileName)
 	if (det == 0)
 	{
 		std::cout << "Impossible to calculate inverted matrix (det = 0)." << std::endl;
-		return false;
+		wasError = true;
+		return matrix;
 	}
 	
 	//computing InvertedMatrix
-	double invertedMatrix[3][3];
+	Matrix3x3 invertedMatrix;
 	for (size_t i = 0; i < 3; i++)
 	{
 		for (size_t j = 0; j < 3; j++)
@@ -125,10 +139,43 @@ bool ComputeInvertedMatrix(const std::string & fileName)
 			invertedMatrix[j][i] = ComputeCellOfInvertedMatrix(matrix, det, i, j);
 		}
 	}
+	return invertedMatrix;
+}
 
-	PrintMatrix(invertedMatrix);
+
+bool WriteMatrixInFile(const Matrix3x3 & matrix)
+{
+	std::ofstream outputFile("output.txt");
+	if (!outputFile.is_open())
+	{
+		return false;
+	}
+
+	for (size_t i = 0; i < matrix.size(); i++)
+	{		
+		for (size_t j = 0; j < matrix[i].size(); j++)
+		{
+			outputFile << std::fixed << std::setprecision(3) << matrix[i][j];
+			if (j < 2)
+			{
+				outputFile << " ";
+			}
+		}
+		if (i < 2)
+		{
+			outputFile << '\n';
+		}
+	}
+
+	if (!outputFile.flush())
+	{
+		std::cout << "Failed to write in output file";
+		return false;
+	}
+
 	return true;
 }
+
 
 int main(int argc, char * argv[])
 {
@@ -137,9 +184,14 @@ int main(int argc, char * argv[])
 		std::cout << "incorrect program execution.\nUsage: invert.exe <fileName>" << std::endl;
 		return 1;
 	}
-	if (!ComputeInvertedMatrix(argv[1]))
+	
+	bool isError = false;
+	Matrix3x3 resultMatrix = ComputeInvertedMatrix(argv[1], isError);
+	if(isError)
 	{
 		return 1;
 	}
+	PrintMatrix(resultMatrix);
+	WriteMatrixInFile(resultMatrix);
 	return 0;
 }
--- invert/test.bat
+++ invert/test.bat
@@ -2,24 +2,37 @@ REM
 invert.exe input.txt output.txt
 IF NOT ERRORLEVEL 1 GOTO err
 
+REM
 invert.exe test_successful1.txt
 IF ERRORLEVEL 1 GOTO err
-
-invert.exe test_successful2.txt
+FC /B output.txt test_successful1_out.txt
 IF ERRORLEVEL 1 GOTO err
 
+REM
+invert.exe test_successful2.txt
+IF ERRORLEVEL 1 GOTO err
+FC /B output.txt test_successful2_out.txt
+IF ERRORLEVEL 1 GOTO err
+
+REM
 invert.exe test_incorrect1.txt
 IF NOT ERRORLEVEL 1 GOTO err
 
+REM
 invert.exe test_incorrect2.txt
 IF NOT ERRORLEVEL 1 GOTO err
 
+REM
 invert.exe test_incorrect3.txt
 IF NOT ERRORLEVEL 1 GOTO err
 
+REM
 invert.exe test_incorrect4.txt
 IF NOT ERRORLEVEL 1 GOTO err
 
+REM
+invert.exe test_incorrect5.txt
+IF NOT ERRORLEVEL 1 GOTO err
 
 ECHO Tests are finished successfully.
 EXIT /B
